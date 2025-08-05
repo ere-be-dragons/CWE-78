4c50e47c532bae6263b5d297d16789a3e426efe2
Lawrence <lawrence@acode.ninja>
2025-08-05 16:00:32 +0100

    feat: application with command injection vuln

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import chalk from 'chalk';
+import express from 'express'
+import handlebars from 'express-handlebars';
+import {exec} from 'node:child_process';
+
+const app = express();
+
+app.engine('html', handlebars.engine({
+  extname: '.html',
+}));
+app.set('views', './src/views');
+app.set('view engine', 'html');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/cmd/ping', (req, res) => {
+  exec(`dig ${req.query.ip_addr}`, (err, stdout) => {
+    res.render('home', {output: err?.toString() ?? stdout.toString()});
+
+    console.log(
+      chalk.blue('[tries]\t'),
+      stdout.toString().includes(process.env.DATABASE_URL) ? '🟢 ' : '🟥 ',
+      req.query.ip_addr,
+    );
+  });
+});
+
+app.listen(process.env.PORT ?? '5000', () => {
+  console.log(chalk.blue('[start]\t'), `available on http://localhost:${process.env.PORT ?? 5000}`);
+  console.log(chalk.blue('[tries]\t'), 'waiting for injection attempts...');
+});
